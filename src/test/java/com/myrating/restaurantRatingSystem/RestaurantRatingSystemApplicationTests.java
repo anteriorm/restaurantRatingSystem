477e188b0e7f0cb88948636a343ac2f1e12d0ad3
User <klmisha333@mail.ru>
2025-07-13 18:10:20 +0400

    Spring Boot project generated

---
+++ src/test/java/com/myrating/restaurantRatingSystem/RestaurantRatingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myrating.restaurantRatingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantRatingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
